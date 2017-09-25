3cf4501147eb02ac3857c3bfa3d80d42e0798953
Megan Miller <meganmiller@Megans-MacBook-Pro.local>
2017-09-25 17:35:27 -0600

    Finished

--- src/components/WizardFive/WizardFive.js
+++ src/components/WizardFive/WizardFive.js
@@ -1,17 +1,21 @@
 import React,  { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import {connect} from 'react-redux';
+import {updateAgent} from '../../ducks/reducer'
+
 class WizardFive extends Component {
 
     render(){
+        const { updateAgent } = this.props;
         return(
             <div className="parent-div">
                     <div className="vert-align">    
 
                     <p>Are you currently working with a real estate agent?</p> <br />
                     <div className="row">
-                        <Link to="/wSix"><button onClick={this.props.realEstateAgentTrue}>Yes</button></Link>
-                        <Link to="/wSix"><button onClick={this.props.realEstateAgentFalse}>No </button></Link>
+                        <Link to="/wSix"><button onClick={() => updateAgent('Yes')}>Yes</button></Link>
+                        <Link to="/wSix"><button onClick={() => updateAgent('No')}>No </button></Link>
                     </div>
                 </div>
             </div>
@@ -19,4 +23,16 @@ class WizardFive extends Component {
     }
 }
 
-export default WizardFive;
+function mapStateToProps(state) {
+    return {
+        realEstateAgent: state.realEstateAgent
+    }
+}
+
+let outActions = {
+    updateAgent
+}
+
+let connected = connect(mapStateToProps, outActions)
+
+export default connected(WizardFive);
--- src/components/WizardFour/WizardFour.js
+++ src/components/WizardFour/WizardFour.js
@@ -1,16 +1,20 @@
 import React,  { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import {connect} from 'react-redux';
+import {updateFound} from '../../ducks/reducer';
+
 class WizardFour extends Component {
     render(){
+        const { updateFound } = this.props;
         return(
             <div className="parent-div">
                 <div className="vert-align">            
                     <p>Have you already found your new home?</p> <br />
                     
                     <div className="row">
-                        <Link to="/wFive"><button onClick={this.props.foundTrue}>Yes</button></Link>
-                        <Link to="/wFive"><button onClick={this.props.foundFalse}>No </button></Link>  
+                        <Link to="/wFive"><button onClick={() => updateFound('Yes')}>Yes</button></Link>
+                        <Link to="/wFive"><button onClick={() => updateFound('No')}>No </button></Link>  
                     </div>         
                 </div>
             </div>
@@ -18,4 +22,16 @@ class WizardFour extends Component {
     }
 }
 
-export default WizardFour;
+function mapPropsToState(state) {
+    return {
+        found: state.found
+    }
+}
+
+let outActions = {
+    updateFound
+}
+
+let connected = connect(mapPropsToState, outActions)
+
+export default connected(WizardFour);
